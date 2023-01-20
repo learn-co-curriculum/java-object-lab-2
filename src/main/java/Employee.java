30d41769390ea5c335553c716a53618beed0e62a
Linda Seiter <linseiter@gmail.com>
2023-01-20 09:50:45 -0500

    new object lab#2 covering access and non-access modifiers

---
+++ src/main/java/Employee.java
@@ -0,0 +1,20 @@
+public class Employee {
+
+    // Add fields
+
+
+    public static void main(String[] args) {
+
+        // Create Employee instances
+
+
+        // Sasha works 14 hours
+
+
+        // Tal works 26 hours
+
+
+        // Define format string and print employee information
+
+    }
+}
